8b8d2e3f7da3548a7bbe3d6f0502474b418251d4
Matias Tanans <matias.tanans@gmail.com>
2023-07-09 18:58:18 -0300

    Contador creado

---
+++ src/components/common/Counter/Counter.jsx
@@ -0,0 +1,36 @@
+import { Button } from "@mui/material";
+
+const Counter = ({ counter, setCounter, agregarAlCarrito, stock }) => {
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <button
+          disabled={counter <= 1}
+          variant="contained"
+          onClick={() => setCounter(counter - 1)}
+        >
+          -
+        </button>
+
+        <h2>{counter}</h2>
+        <button
+          disabled={counter >= stock}
+          variant="contained"
+          onClick={() => setCounter(counter + 1)}
+        >
+          +
+        </button>
+      </div>
+      <br />
+      <button
+        variant="contained"
+        color="secondary"
+        onClick={() => agregarAlCarrito(counter)}
+      >
+        Agregar al Carrito
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
