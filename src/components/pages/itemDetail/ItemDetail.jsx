523e3db3d618e33fc01bdb7181b8bac8229195ef
Matias Tanans <matias.tanans@gmail.com>
2023-07-24 18:49:37 -0300

    Dando estilos a los detalles de los items.

--- src/components/pages/itemDetail/ItemDetail.jsx
+++ src/components/pages/itemDetail/ItemDetail.jsx
@@ -3,16 +3,21 @@ import "./ItemDetail.css";
 
 const ItemDetail = ({ product, agregarAlCarrito }) => {
   return (
-    <div>
-      <h1>{product.title}</h1>
-      <h3>{product.price}</h3>
-      <h3>{product.description}</h3>
+    <>
+      <div className="detailContainer">
+        <h1 className="title">{product.title}</h1>
+        <img src={`${product.img}`} alt="Portada" />
+        <h3 className="price">$ {product.price}</h3>
+        <h3 className="description">{product.description}</h3>
+      </div>
 
-      <CounterContainer
-        stock={product.stock}
-        agregarAlCarrito={agregarAlCarrito}
-      />
-    </div>
+      <div className="counterContainer">
+        <CounterContainer
+          stock={product.stock}
+          agregarAlCarrito={agregarAlCarrito}
+        />
+      </div>
+    </>
   );
 };
 
