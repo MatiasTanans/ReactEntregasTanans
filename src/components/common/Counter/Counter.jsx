58813937e9ffc85572f885b2e69806b66b4c45be
Matias Tanans <matias.tanans@gmail.com>
2023-08-02 18:27:20 -0300

    Editando Counter

--- src/components/common/Counter/Counter.jsx
+++ src/components/common/Counter/Counter.jsx
@@ -3,30 +3,21 @@ import "./Counter.css";
 const Counter = ({ counter, setCounter, agregarAlCarrito, stock }) => {
   return (
     <div>
-      <div style={{ display: "flex" }}>
-        <button
-          disabled={counter <= 1}
-          variant="contained"
-          onClick={() => setCounter(counter - 1)}
-        >
+      <div>
+        <button disabled={counter <= 1} onClick={() => setCounter(counter - 1)}>
           -
         </button>
 
         <h2>{counter}</h2>
         <button
           disabled={counter >= stock}
-          variant="contained"
           onClick={() => setCounter(counter + 1)}
         >
           +
         </button>
       </div>
       <br />
-      <button
-        variant="contained"
-        color="secondary"
-        onClick={() => agregarAlCarrito(counter)}
-      >
+      <button onClick={() => agregarAlCarrito(counter)}>
         Agregar al Carrito
       </button>
     </div>
